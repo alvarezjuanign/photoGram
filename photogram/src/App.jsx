1ba2728897d1571e0e7479c8a141b5681b7a26f4
Juan Alvarez <alvarez.juanign@gmail.com>
2024-12-06 15:26:50 -0300

    Add top navbar

--- photogram/src/App.jsx
+++ photogram/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import message from './assets/message.svg'
+import heart from './assets/heart.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Navbar } from './components/Navbar'
 
+export function App() {
   return (
-    <main className='bg-slate-800'>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Hola {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </main>
+    <>
+      <nav className='flex justify-between'>
+        <h1 className='text-2xl m-1'>PhotoGram</h1>
+        <div className='flex mx-3 gap-2'>
+          <a href='/notificaciones' className='mt-1'>
+            <img src={heart} alt='' className='w-6 cursor-pointer' />
+          </a>
+          <a href='/mensajes' className='mt-1'>
+            <img src={message} alt='' className='w-6 cursor-pointer' />
+          </a>
+        </div>
+      </nav>
+      <main>
+        <h2 className='text-2xl m-1'>Fotos</h2>
+        <Navbar />
+      </main>
+    </>
   )
 }
-
-export default App
