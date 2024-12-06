d298543e3aa71c0ce14990fcef3cb72ff61ca2a6
Juan Alvarez <alvarez.juanign@gmail.com>
2024-12-06 15:26:12 -0300

    Add bottom navbar

---
+++ photogram/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import plus from '../assets/plus.svg'
+import home from '../assets/home.svg'
+import glass from '../assets/glass.svg'
+import reel from '../assets/reel.svg'
+
+export function Navbar() {
+  return (
+    <section className='sticky bottom-0 bg-white'>
+      <nav>
+        <ul className='flex justify-around items-center h-10'>
+          <li>
+            <a href='#'><img src={home} alt='Icono home' className='h-5' /></a>
+          </li>
+          <li>
+            <a href='#'><img src={glass} alt='Icono buscador' className='h-5' /></a>
+          </li>
+          <li>
+            <a href='#'><img src={plus} alt='Icono crear' className='h-5' /></a>
+          </li>
+          <li>
+            <a href='#'><img src={reel} alt='Icono reel' className='h-5' /></a>
+          </li>
+          <li>
+            <a href='./perfil'>
+              <img
+                src='https://i.pravatar.cc/150?img=11'
+                alt='Foto de perfil de usuario'
+                className='rounded-full h-5 border'
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  )
+}
